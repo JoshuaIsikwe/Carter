89cc36ee52217cd2b6489a93703b6c6aa9a6bd4b
JoshuaIsikwe <IsikweJoshua@gmail.com>
2022-03-09 01:29:49 +0200

    Fetched mutiple pages from the api

--- src/components/films/Films.jsx
+++ src/components/films/Films.jsx
@@ -8,9 +8,16 @@ class Films extends Component {
     this.state = {movies: []};
   }
 
+   addMore = async () => {
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=d4f7b87d7cedfdfbbb297f46aa3e8779&language=en-US&page=2");
+    const data = await response.json();
+    this.setState({ 
+      movies: [...this.state.movies,...data.results]
+    });
+  }
+
   async componentDidMount() {
     const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=d4f7b87d7cedfdfbbb297f46aa3e8779&language=en-US&page=1");
-    
     const data = await response.json();
     this.setState({movies: data.results});
   }
@@ -18,17 +25,22 @@ class Films extends Component {
   render() {
     const movies = this.state.movies
     const movieList = movies.map((movie) =>
-    <div className="movies-card">
-    <img className='movie-img' src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
-    <div className="movie-title">
-        <h5>{movie.title}</h5>
+
+        <div className="movies-card">
+          <img className='movie-img' src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+          
+          <div className="movie-title">
+              <h5>{movie.title}</h5>
+            </div>
       </div>
-  </div>
 );
     return (
-      <div className='movies'> 
-        {movieList}
-    </div>
+      <div> 
+        <div className='movies'>{movieList}</div>
+        <div className="add-more">
+            <button onClick={this.addMore}>Add more</button>
+          </div>
+      </div>
     );
   }
 }
