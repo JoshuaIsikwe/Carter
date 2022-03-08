cf66a9c7cddde04a922f5973bda3be1add6ecce4
JoshuaIsikwe <IsikweJoshua@gmail.com>
2022-03-08 16:29:19 +0200

    Made API calls to get and display movies

--- src/components/films/Films.jsx
+++ src/components/films/Films.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import "./films.css"
 
-const Films = () => {
-  return (
-    <div className='movies'> 
-      <div className="movies-card">
-          <img className='movie-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMMTcBQEEpgM1clE6PAHkS2FuPo90wcIZ4jQ&usqp=CAU" alt="" />
-          <div className="movie-title">
-              <h5>Dark Knight</h5>
-            </div>
-        </div>
-        <div className="movies-card">
-          <img className='movie-img' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMMTcBQEEpgM1clE6PAHkS2FuPo90wcIZ4jQ&usqp=CAU" alt="" />
-          <div className="movie-title">
-              <h5>Dark Knight</h5>
-            </div>
-        </div>
+class Films extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {movies: []};
+  }
+
+  async componentDidMount() {
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=d4f7b87d7cedfdfbbb297f46aa3e8779&language=en-US&page=1");
+    
+    const data = await response.json();
+    this.setState({movies: data.results});
+  }
+
+  render() {
+    const movies = this.state.movies
+    const movieList = movies.map((movie) =>
+    <div className="movies-card">
+    <img className='movie-img' src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt="" />
+    <div className="movie-title">
+        <h5>{movie.title}</h5>
+      </div>
+  </div>
+);
+    return (
+      <div className='movies'> 
+        {movieList}
     </div>
-  )
+    );
+  }
 }
 
-export default Films
+Films.propTypes = {
+
+};
+
+export default Films;
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 import "./home.css"
 
-const Home = () => {
-  return (
-    <div>
-        <div className='home'>
-      <div className="home-container">
-        <video src="/videos/car_video.mp4" autoPlay loop muted ></video>
-        <h1>Welcome to Fantastic Movies</h1>
-        <p>We have a wide selection of films to watch</p>
-        <div className=''>
-          <button className='button-37'  role="button">Browse Films</button>
-        </div>
-      </div>
-    </div>
-    </div>
-  )
-}
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {movies: []};
+  }
 
-export default Home
+  async componentDidMount() {
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=d4f7b87d7cedfdfbbb297f46aa3e8779&language=en-US&page=1");
+    
+    const data = await response.json();
+    this.setState({movies: data.results});
+  }
+
+
+  render() {
+    return (
+      <div>
+            <div className='home'>
+          <div className="home-container">
+           
+            <h1>Welcome to Fantastic Movies</h1>
+            <p>We have a wide selection of films to watch</p>
+            <div className=''>
+              <button className='button-37'  role="button">Browse Films</button>
+            </div>
+          </div>
+        </div>
+    </div>
+    )
+  }
+}
